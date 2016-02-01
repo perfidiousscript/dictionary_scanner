9148d842204a3c4b2f31bf6e508c1cbd25e4fa4b
Sam Moss <sam.moss321@gmail.com>
2016-02-01 16:46:05 -0600

    functional with trivial dictionary

--- script.js
+++ script.js
@@ -3,13 +3,48 @@
  */
 var trivialDictionary = ["arrows", "carrots", "give", "me"];
 var wordObject = {};
+var solutionObject = {};
 
-
-var populateObject = function(wordArray){
+//This function populates the wordObject
+// with an object for every word
+// greater than four letters
+// in the dictionary.
+var iterateThroughWords = function(wordArray){
     for(var i = 0; i < wordArray.length; i++){
-        wordObject[wordArray[i]] = {}
+        if(wordArray[i].length > 3){
+            wordObject[wordArray[i]] = {};
+            var currentWordObject = wordObject[wordArray[i]];
+            currentWordObject.letters = wordArray[i].split("");
+            currentWordObject.sequences = [];
+            currentWordObject.word = wordArray[i];
+            assembleSequences(currentWordObject);
+        }
     }
     console.log("here is the word object: ",wordObject);
+    console.log("here is the solution object: ",solutionObject);
 };
 
-populateObject(trivialDictionary);
+var assembleSequences = function(currentWordObject){
+    for(var j = 3; j < currentWordObject.letters.length; j++){
+        var letterList = currentWordObject.letters;
+        var currentSequence = letterList[j - 3] + letterList[j - 2] + letterList[j - 1] + letterList[j];
+        currentWordObject.sequences.push(currentSequence);
+        checkForDuplicate(currentSequence, currentWordObject);
+    }
+};
+
+var checkForDuplicate = function(newSequence, referenceWord){
+   for(oldSequence in solutionObject){
+       if(oldSequence.sequence === newSequence) {
+           delete solutionObject[oldSequence];
+           return;
+       }
+   }
+    solutionObject[newSequence] = {};
+    solutionObject[newSequence].sequence = newSequence;
+    solutionObject[newSequence].word = referenceWord.word;
+};
+
+var printSolution
+
+iterateThroughWords(trivialDictionary);
