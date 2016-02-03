fb501170b01ff5c6cae0316c6c45d3c2b64aa7e8
Sam Moss <sam.moss321@gmail.com>
2016-02-03 12:52:21 -0600

    mocha and chai hook up

---
+++ tests/test.js
@@ -0,0 +1,8 @@
+var assert = require('chai').assert;
+
+
+describe('dictionary scanner',function(){
+    it('should return an alphabetical array of sequences',function(){
+        assert.equal(1+1,2);
+    });
+});
