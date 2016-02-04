d1bf2862f56d34d6e7df852bed57d85cca4d3758
Sam Moss <sam.moss321@gmail.com>
2016-02-04 10:47:15 -0600

    tests for odd and even number of sequences

--- test/test.js
+++ test/test.js
@@ -8,6 +8,12 @@ describe('dictionary scanner',function(){
     it('should work on a single sequence',function(){
         assert.deepEqual(script.execute('oooo'),{finalSequenceList:['oooo'],finalWordList:['oooo']});
     });
+    it('should work on an even number of sequences',function(){
+        assert.deepEqual(script.execute('oooo\noooo'),{finalSequenceList:[],finalWordList:[]});
+    });
+    it('should work on an odd number of sequences',function(){
+        assert.deepEqual(script.execute('oooo\noooo\noooo'),{finalSequenceList:[],finalWordList:[]});
+    });
     it('should work on the trivial dictionary',function(){
         assert.deepEqual(script.execute("arrows\ncarrots\ngive\nme"),{ finalSequenceList: [ 'carr', 'give', 'rots', 'rows', 'rrot', 'rrow' ],
             finalWordList: [ 'carrots', 'give', 'carrots', 'arrows', 'carrots', 'arrows' ] });
